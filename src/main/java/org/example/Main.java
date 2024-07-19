780db816dbefa96f44b4ccccb5e76705a5da78bf
1-21-01-E043-09\POO <santiago.cumbalepn.edu.ec>
2024-07-19 11:45:36 -0500

    xd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,52 @@
 package org.example;
 
+import javax.swing.*;
+import java.sql.*;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       /* String url="jdbc:mysql://localhost:3306/Estudiantes2024a";
+        String user="root";
+        String password="123456";
+        Scanner sc=new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+
+        try(Connection connection= DriverManager.getConnection(url,user,password)){
+            System.out.println("Conexion exitosa");
+            System.out.println("Ingrese el numero de cedula del estudiante: ");
+            String opcion=sc.nextLine();
+            String query="select *, round((b1+b2)/2,2) as promedio from estudiantes where cedula= '"+opcion+"'";
+            Statement statement=connection.createStatement();
+            ResultSet resultSet=statement.executeQuery(query);
+
+
+            while(resultSet.next()){
+                System.out.print("El estudiante "+resultSet.getString("nombre")+" con numero de cedula: "+resultSet.getString("cedula")+" tiene las siguientes notas: "+resultSet.getString("b1")+" - "+resultSet.getString("b2")+" y su promedio es: "+resultSet.getString("promedio"));
+                System.out.println();
+                float promedio=resultSet.getFloat("promedio");
+
+                if(promedio>14){
+                    System.out.println("El estudiante paso el semestre");
+                }else {
+                    float supletorios=14-promedio;
+                    float supletoriosR=Math.round(supletorios);
+                    System.out.println("El estudiante necesita: "+supletoriosR+" para pasar el semestre");
+                }
+
+            }
         }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }*/
+        JFrame frame = new JFrame("Inicia Sesion");
+        frame.setContentPane(new form1().MainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,200);
+        frame.setVisible(true);
+
     }
 }
--- src/main/java/org/example/form1.java
+++ src/main/java/org/example/form1.java
@@ -1,2 +1,39 @@
-package org.example;public class form1 {
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.Scanner;
+
+public class form1 {
+    private JButton button1;
+    public JPanel MainPanel;
+    private JLabel Texto;
+    String url="jdbc:mysql://localhost:3306/Estudiantes2024a";
+    String user="root";
+    String password="123456";
+    Scanner sc=new Scanner(System.in);
+
+
+    public form1() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try(Connection connection= DriverManager.getConnection(url,user,password)){
+                    System.out.println("Conexion exitosa");
+                    String query="select * from estudiantes";
+                    Statement statement=connection.createStatement();
+                    ResultSet resultSet=statement.executeQuery(query);
+                    while(resultSet.next()){
+                        Texto.setText(resultSet.getString("nombre"));
+                    }
+                }
+                catch(
+                        SQLException e1){
+                    System.out.println(e1.getMessage());
+                }
+            }
+        });
+    }
 }
